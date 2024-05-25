7c3ff4f383e9c655d2ce40d1de15883810bbd155
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-05-25 22:18:41 +0330

    minor: fix comment

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,4 @@
-/*
-Copyright © 2024 Motalleb Fallahnezhad
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+// Package cmd contains cli flags and commands
 package cmd
 
 import (
@@ -21,8 +6,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -59,5 +42,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
