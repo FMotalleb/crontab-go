5b74c7fe32900cc34e813e4134e693b862107c85
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-06-03 00:12:55 +0330

    feat: `interval` scheduler implemented

---
+++ core/schedule/interval.go
@@ -0,0 +1,59 @@
+package schedule
+
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/FMotalleb/crontab-go/abstraction"
+)
+
+func test() {
+	sh := &Interval{}
+	tester(sh)
+}
+
+func tester(sh abstraction.Scheduler) {}
+
+type Interval struct {
+	duration   time.Duration
+	logger     *logrus.Entry
+	ticker     *time.Ticker
+	notifyChan chan any
+}
+
+func NewInterval(schedule time.Duration, logger logrus.Entry) Interval {
+	return Interval{
+		duration: schedule,
+		logger: logger.
+			WithFields(
+				logrus.Fields{
+					"scheduler": "interval",
+					"interval":  schedule,
+				},
+			),
+	}
+}
+
+// buildTickChannel implements abstraction.Scheduler.
+func (c *Interval) BuildTickChannel() <-chan any {
+	c.Cancel()
+	c.notifyChan = make(chan any)
+	c.ticker = time.NewTicker(c.duration)
+	go func() {
+		for range c.ticker.C {
+			c.notifyChan <- false
+		}
+	}()
+
+	return c.notifyChan
+}
+
+// cancel implements abstraction.Scheduler.
+func (c *Interval) Cancel() {
+	if c.ticker != nil {
+		c.logger.Debugln("scheduler cancel signal received for an active instance")
+		c.ticker.Stop()
+		close(c.notifyChan)
+	}
+}
