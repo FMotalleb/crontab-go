11c477bc8d7016d61b85fffc286f06541fc7a6d6
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-06-05 23:49:35 +0330

    feat: fully functional command implementation

--- core/task/command.go
+++ core/task/command.go
@@ -1,75 +1,108 @@
 package task
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/sirupsen/logrus"
 
 	"github.com/FMotalleb/crontab-go/abstraction"
 	"github.com/FMotalleb/crontab-go/cmd"
+	"github.com/FMotalleb/crontab-go/config"
 )
 
 type Command struct {
 	exe              string
 	envVars          *[]string
 	workingDirectory string
-	logger           *logrus.Entry
-	proc             *exec.Cmd
+	log              *logrus.Entry
+	cancel           context.CancelFunc
+
+	retries    uint
+	retryDelay time.Duration
+	timeout    time.Duration
 }
 
 // Cancel implements abstraction.Executable.
 func (g *Command) Cancel() {
-	if g.proc != nil {
-		g.logger.Debugln("canceling executable")
-		e := g.proc.Cancel()
-		if e != nil {
-			g.logger.Warnln("cannot stop process", e)
-		}
+	if g.cancel != nil {
+		g.log.Debugln("canceling executable")
+		g.cancel()
 	}
 }
 
 // Execute implements abstraction.Executable.
-func (g *Command) Execute() (e error) {
-	g.Cancel()
-
-	proc := exec.Cmd{}
-	proc.Args = append(cmd.CFG.ShellArgs, *&g.exe)
-	proc.Env = *g.envVars
-	proc.Path = cmd.CFG.Shell
-	proc.Dir = g.workingDirectory
-
-	res, e := proc.CombinedOutput()
-	g.logger.Infoln("command finished with answer:\n", string(res))
-	if e != nil {
-		g.logger.Warn("failed to start the command", e)
-		return
+func (g *Command) Execute(ctx context.Context) (e error) {
+	r := getRetry(ctx)
+	log := g.log.WithField("retry", r)
+	if getRetry(ctx) > g.retries {
+		log.Warn("maximum retry reached")
+		return fmt.Errorf("maximum retries reached")
 	}
-
+	if r != 0 {
+		log.Debugln("waiting", g.retryDelay, "before executing the next iteration after last fail")
+		time.Sleep(g.retryDelay)
+	}
+	ctx = increaseRetry(ctx)
+	var procCtx context.Context
+	var cancel context.CancelFunc
+	if g.timeout != 0 {
+		procCtx, cancel = context.WithTimeout(ctx, g.timeout)
+	} else {
+		procCtx, cancel = context.WithCancel(ctx)
+	}
+	g.cancel = cancel
+	proc := exec.CommandContext(
+		procCtx,
+		cmd.CFG.Shell,
+		append(cmd.CFG.ShellArgs, *&g.exe)...,
+	)
+	proc.Env = *g.envVars
+	proc.Dir = g.workingDirectory
+	var res bytes.Buffer
+	proc.Stdout = &res
+	proc.Stderr = &res
+	proc.Start()
+	e = proc.Wait()
+	log.Infof("command finished with answer: `%s`", strings.TrimSpace(string(res.Bytes())))
+	if e != nil {
+		log.Warn("failed to execute the command ", e)
+		return g.Execute(ctx)
+	}
 	return
 }
 
 func NewCommand(
-	exe string,
-	envVars *map[string]string,
-	workingDirectory string,
-	logger logrus.Entry,
+	task config.Task,
+	logger *logrus.Entry,
 ) abstraction.Executable {
+	log := logger.WithField("command", task.Command)
 	env := os.Environ()
-	for key, val := range *envVars {
+	for key, val := range task.Env {
 		env = append(env, fmt.Sprintf("%s=%s", key, val))
 	}
+	wd := task.WorkingDirectory
+	if wd == "" {
+		var e error
+		wd, e = os.Getwd()
+		if e != nil {
+			logger.Fatalln("cannot get current working directory: ", e)
+		}
+	}
 	return &Command{
-		exe,
-		&env,
-		workingDirectory,
-		logger.WithFields(
-			logrus.Fields{
-				"exe":               exe,
-				"working_directory": workingDirectory,
-			},
+		exe:              task.Command,
+		envVars:          &env,
+		workingDirectory: wd,
+		log: log.WithField(
+			"working_directory", wd,
 		),
-		nil,
+		retries:    task.Retries,
+		retryDelay: task.RetryDelay,
+		timeout:    task.Timeout,
 	}
 }
