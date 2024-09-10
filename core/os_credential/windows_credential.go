133e83b2b8da070a665817ac3c7bb23f340b55c3
FMotalleb <m.falahnejad@dornica.net>
2024-09-10 09:21:17 +0330

    fix: disabled verbose logging on setting user on windows

--- core/os_credential/windows_credential.go
+++ core/os_credential/windows_credential.go
@@ -10,11 +10,15 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// NOOP if user and group are empty log a warning if not and always returns nil
 func Validate(log *logrus.Entry, usr string, grp string) error {
+	if usr == "" && grp == "" {
+		return nil // skip warn message if no user and group provided
+	}
 	log.Warn("windows os does not have capability to set user thus validation will pass but will not work")
 	return nil
 }
 
+// NOOP
 func SetUser(log *logrus.Entry, _ *exec.Cmd, _ string, _ string) {
-	log.Warn("cannot set user in windows platform")
 }
